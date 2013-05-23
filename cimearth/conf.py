3091f7f513f1592ed5250dd61e361667ac7065d0
Nathan Matteson <matteson@obstructures.org>
2013-05-23 08:20:59 -0500

    Begin module

---
+++ cimearth/conf.py
@@ -0,0 +1,14 @@
+VECTORS = (
+    'region',
+    'commodity',
+    'consumer',
+)
+
+DATA_TYPES = (
+    'price',
+    'emissions',
+    'emission_flows',
+    'expend',
+    'limits',
+    'ratio',
+)
