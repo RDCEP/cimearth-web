cb74e451c8031578c3d9e919575ed324d630c80f
Nathan Matteson <matteson@obstructures.org>
2013-05-24 19:22:45 -0500

    Add rollover regions and docstrings

--- static/js/line_graph.js
+++ static/js/line_graph.js
@@ -14,11 +14,17 @@ function CimEarthLineGraph() {
     height = inner_height - padding.top - padding.bottom,
     offset = 0
   ;
-  var start_year = 2004, end_year = 2024;
-  var min_d, max_d, min_domain, max_domain;
-  var x = d3.scale.linear().domain([start_year, end_year]).range([0, width]),
+  /*
+   Data domain
+   */
+  var start_year = 2004, end_year = 2024,
+    min_d, max_d, min_domain, max_domain,
+    x = d3.scale.linear().domain([start_year, end_year]).range([0, width]),
     y = d3.scale.linear().domain([1, 0]).range([0, height])
   ;
+  /*
+   Graph entities
+   */
   var svg = d3.select("#graph").append("svg")
       .attr("width", width + padding.left + padding.right)
       .attr("height", height + padding.top + padding.bottom)
@@ -43,9 +49,16 @@ function CimEarthLineGraph() {
     color,
     dots_layers,
     paths,
-    dots
+    dots,
+    points
   ;
+  /*
+   End variables
+   */
   function flat_data(d, r, _i) {
+    /*
+     Combine datum, region, and index into one object for rollover circles
+     */
     _d = [];
     for (i=0; i<d.length; ++i) {
       _d.push({data: d[i], region: r, color: _i});
@@ -53,6 +66,9 @@ function CimEarthLineGraph() {
     return _d;
   }
   function get_domain(data) {
+    /*
+     Update y domain based on updated data min and max
+     */
     min_d = d3.min(data, function(d) { return d3.min(d.data); });
     max_d = d3.max(data, function(d) { return d3.max(d.data); });
     max_domain = max_d + ((max_d - min_d) / 10);
@@ -60,39 +76,51 @@ function CimEarthLineGraph() {
     return [max_domain, min_domain];
   }
   this.redraw = function(dt, r1, r2, c1) {
+    /*
+     Update graphs on <select> changes.
+     */
     $.ajax({
       dataType: 'json',
       url: 'http://cimearth.obstructures.org/json/'+dt+'/'+r1+'/'+r2+'/'+c1,
       success: function(data) {
-        $('#ajax-loader').remove();
-      color = d3.scale.category10()
-        .domain(d3.keys(data).filter(function(key) { return key === "region"; }));
-      y.domain(get_domain(data));
-      regions.data(data);
-      paths.data(data)
-        .transition()
-        .duration(200)
-        .attr("d", function(d) {return line(d.data)})
-        .style("stroke", function(d, i) { return color(i); })
-      ;
-      dots_layers.data(data);
-      dots.data(function(d, i) { return flat_data(d.data, d.region, i); })
-        .attr('cx', function(d, i) { return x(i+start_year); })
-        .attr('cy', function(d) { return y(d.data); })
-        .attr('class', function(d, i) {
-          return 'dot-' + i;
+        points = d3.max(data, function(d, i) {
+          return d.data.length;
         })
-        .classed('dot', true)
-      ;
-      draw_axes();
+        color = d3.scale.category10()
+          .domain(d3.keys(data).filter(function(key) { return key === "region"; }));
+        y.domain(get_domain(data));
+        draw_axes();
+        regions.data(data);
+        paths.data(data)
+          .transition()
+          .duration(200)
+          .attr("d", function(d) {return line(d.data)})
+          .style("stroke", function(d, i) { return color(i); })
+        ;
+        dots_layers.data(data);
+        dots.data(function(d, i) { return flat_data(d.data, d.region, i); })
+          .attr('cx', function(d, i) { return x(i+start_year); })
+          .attr('cy', function(d) { return y(d.data); })
+          .attr('class', function(d, i) {
+            return 'dot-' + i;
+          })
+          .classed('dot', true)
+        ;
       }
     });
   };
   this.draw = function() {
+    /*
+     Draws initial graph on page load. Builds hover effects.
+     */
     d3.json('http://cimearth.obstructures.org/json/price/USA/MEX/OIL/', function(error, data) {
+      points = d3.max(data, function(d, i) {
+          return d.data.length;
+      })
       color = d3.scale.category10()
         .domain(d3.keys(data).filter(function(key) { return key === "region"; }));
       y.domain(get_domain(data));
+      draw_axes();
       regions = svg.selectAll('regions')
         .data(data)
         .enter()
@@ -118,6 +146,19 @@ function CimEarthLineGraph() {
           return 'dot-' + i;
         })
         .classed('dot', true)
+      ;
+      svg.selectAll('.data-region')
+        .data(data[0].data)
+        .enter()
+        .append('rect')
+        .style('fill', 'none')
+        .style('pointer-events', 'all')
+        .attr('width', width / (points - 1))
+        .attr('height', height)
+        .attr('x', function(d, i) {
+          return (i * width / (points - 1)) - (width / (points - 1) / 2);
+        })
+        .attr('y', 0)
         .on('mouseover', function(d, i) {
           d3.selectAll('.dot-'+i)
             .style('fill', function(dd, ii) { return color(dd.color); })
@@ -135,19 +176,20 @@ function CimEarthLineGraph() {
               .style('left', (d3.event.pageX + 10) + 'px')
               .style('top', (d3.event.pageY - 48) + 'px')
               .style('opacity', .8);
-            })
-        .on('mouseout', function(d, i) {
+        })
+      .on('mouseout', function(d, i) {
           d3.selectAll('.dot-'+i)
             .style('fill', 'none')
             .classed('visible', false);
             tooltip.html('')
             .style('opacity', 0);
-        })
-      ;
-      draw_axes();
+        });
     });
   };
   function draw_axes() {
+    /*
+     Draw graph axes. Called from draw() and redraw().
+     */
     svg.selectAll('.axis').remove();
     svg.append("g")
       .attr("class", "x axis")
@@ -158,5 +200,8 @@ function CimEarthLineGraph() {
       .call(y_axis_ticks);
   }
 }
+/*
+ Instantiate graph and draw on page load.
+ */
 var line_graph = new CimEarthLineGraph();
 line_graph.draw();
