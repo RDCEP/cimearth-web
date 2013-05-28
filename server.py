e287cc6b22ebb54557fd0d3c94c008ea2335106d
Nathan Matteson <matteson@obstructures.org>
2013-05-27 19:24:59 -0500

    Route individual templates

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, make_response
 from uwsgi_app import app
-from cimearth.dat_to_json import CimEarthData
+from cimearth.dat_to_json import DataTable
 
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = ';khfdalhalyjlahflhvlva'
@@ -9,11 +9,21 @@ app.config['SECRET_KEY'] = ';khfdalhalyjlahflhvlva'
 def index():
     return render_template('index.html')
 
-@app.route('/json/<data_type>/<reg1>/<reg2>/<com>')
-def json(data_type,reg1, reg2, com):
-    _data = CimEarthData(data_type)
-    return _data.BetterJsonOut(
-        regions=(reg1,reg2), commodities=(com,), data_type=data_type
+@app.route('/prices/region')
+def prices_region():
+    return render_template('prices/region.html')
+
+@app.route('/prices/commodity')
+def prices_commodity():
+    return render_template('prices/commodity.html')
+
+@app.route('/json/region/<dtype>/<reg>/<com>')
+@app.route('/json/commodity/<dtype>/<reg>/<com>')
+def json2(dtype, reg, com):
+    _data = DataTable(dtype)
+    _data.panelize()
+    return _data.json_output(
+        regions=reg.split(','), commodities=com.split(',')
     )
 
 if __name__ == '__main__':
