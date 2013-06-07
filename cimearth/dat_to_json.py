f2792fb0b4940e443dd6d78dc3f3cf862b82a803
Nathan Matteson <matteson@obstructures.org>
2013-06-07 09:47:43 -0500

    Remove panleize() method and replace with list comprehensions

--- cimearth/dat_to_json.py
+++ cimearth/dat_to_json.py
@@ -10,18 +10,18 @@ class DataFile(object):
     def __init__(self, data_table, _data_set='BAU', start_year=2004):
         Z = zipfile.ZipFile('./data/%s.zip' % _data_set)
         file_type_map = (
-            ('prices', 'price'),
-            ('prices2', 'price'),
-            ('emissions', 'emissions'),
-            ('emission_flows', 'emission_flows'),
-            ('expend_sector', 'expend'),
-            ('expend_region_import', 'expend'),
-            ('expend_sector_import', 'expend'),
-            ('ratio_sector', 'ratio'),
-            ('ratio_region_import', 'ratio'),
-            ('ratio_sector_import', 'ratio'),
+            ('prices', 'price', None),
+            ('prices2', 'price', None),
+            ('emissions', 'emissions', None),
+            ('emission_flows', 'emission_flows', None),
+            ('expend_sector', 'expend', 'top_left'),
+            ('expend_region_import', 'expend', 'bottom_right'),
+            ('expend_sector_import', 'expend', 'bottom_left'),
+            ('ratio_sector', 'ratio', 'top_left'),
+            ('ratio_region_import', 'ratio', 'bottom_right'),
+            ('ratio_sector_import', 'ratio', 'bottom_left'),
         )
-        _f = [v[1] for i,v in enumerate(file_type_map) if v[0] == data_table][0]
+        _f, self.triple_position = [v[1:] for i,v in enumerate(file_type_map) if v[0] == data_table][0]
         _data = Z.read('%s.dat' % _f)
         _data = re.sub(r'^ +', '', _data) # Kill initial whitespace
         _data = re.sub(r'\n +', '\n', _data) # Kill end of line whitespace
@@ -38,69 +38,65 @@ class DataFile(object):
 
 
 class DataTable(DataFile):
-    def panelize(self):
-        panels = self.get_panel_list()
-        frames = {}
-        col_names = []
-        multi_count = {}
-        multi_new = False
-        multi_prev = 0
-        multi_frames = {}
-        N = 0
-        for i in range(len(panels)):
-            year_index = ''
-            data_items = []
-            for j in range(len(panels[i])):
-                _r = panels[i][j]
-                if i == 0 and j == 0:
-                    N = len(_r)
-                    col_names = _r[1:]
-                if j > 1 and (len(_r) > len(panels[i][j-1])):
-                    split = [
-                        len(panels[i][j-1]), j
-                    ]
-                _r += [np.nan] * (N - len(_r)) # only for fucked up tables
-                if j == 0:
-                    try:
-                        year_index = str(int(_r[0]) + self.start_year)
-                    except ValueError:
-                        self.dims = 4
-                        year_index = str(_r[0])
-                        try:
-                            multi_count[year_index] += 1
-                            if multi_count[year_index] > multi_prev:
-                                multi_prev += 1
-                                multi_new = True
-                            else:
-                                multi_new = False
-                        except KeyError:
-                            multi_count[year_index] = 0
-                else:
-                    data_items.append((
-                        _r[0], np.array(_r[1:], dtype=np.float64)
-                    ))
-            if self.dims == 4 and multi_new:
-                multi_frames[str(self.start_year + multi_prev)] = (
-                    pd.Panel(frames).transpose(1, 0, 2)
-                )
-                frames = {}
-            frames[year_index] = (pd.DataFrame.from_items(
-                data_items, orient='index', columns=col_names
-            ))
-        if self.dims == 4:
-            self.panel = pd.Panel4D(multi_frames).transpose(2, 0, 3, 1)
-        else:
-            self.panel = pd.Panel(frames).transpose(2, 0, 1)
+    def grouper(self, iterable, n, fillvalue=None):
+        "Collect data into fixed-length chunks or blocks"
+        # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+        args = [iter(iterable)] * n
+        return itertools.izip_longest(fillvalue=fillvalue, *args)
+
+    def get_triple_panel(self, position):
+        vs0, vs1, hs0, hs1 = 1, 31, 1, 27
+        if position == 'bottom_left':
+            vs0, vs1, hs0, hs1 = 31, 47, 1, 27
+        elif position == 'bottom_right':
+            vs0, vs1, hs0, hs1 = 31, 47, 27, 43
+        groups = self.grouper([
+            [
+                [c for c in np.array(r.split(','))[range(0, 1) + range(hs0, hs1)]] for r in np.array(p.split('\n'))[range(0, 1) + range(vs0, vs1)]
+            ] for p in sorted(
+                sorted(
+                    self._data.split('\n\n'),
+                    key=lambda x: int(x.split('\n')[0].split(',')[-1])  # Sort by year
+                ),
+                key=lambda x: x.split('\n')[0].split(',')[0]  # Sort by region
+            )
+        ], 21)
+        panels = {}
+        for group in groups:
+            g = np.array(group)
+            panels[g[0, 0, 0]] = pd.Panel(
+                g[:, 1:, 1:].astype(np.float64),
+                items=[y + self.start_year for y in range(len(group))],
+                major_axis=g[0, 1:, 0],
+                minor_axis=g[0, 0, 1:],
+            )
+        return pd.Panel4D(
+            panels
+        ).transpose(1, 0, 2, 3)
+
+    def get_simple_panel(self):
+        group = [
+            [
+                [c for c in np.array(r.split(','))[:]] for r in np.array(p.split('\n'))[:47]
+            ] for p in self._data.split('\n\n')
+        ]
+        g = np.array(group)
+        return pd.Panel(
+            g[:, 1:, 1:].astype(np.float64),
+            items=[y + self.start_year for y in range(len(group))],
+            minor_axis=g[0, 0, 1:], #horiz
+            major_axis=g[0, 1:, 0], #vert
+        ).transpose(2, 0, 1)
 
     def json_output2(self, items=(), minors=(), majors=None):
-        if self.data_table in ['expend_sector', 'ratio_sector']:
-            self.panel = self.panel.ix[:26, :30]
-        if self.data_table in ['expend_sector_import', 'ratio_sector_import']:
-            self.panel = self.panel.ix[:26, 30:]
-        if self.data_table in ['expend_region_import', 'ratio_region_import']:
-            self.panel = self.panel.ix[26:, 30:]
+        if majors is not None:
+            self.panel = self.get_triple_panel(self.triple_position)
+            self.dims = 4
+        else:
+            self.panel = self.get_simple_panel()
+            self.dims = 3
         data = []
-        _reg_sectors = itertools.product(items, minors)
+        # _reg_sectors = itertools.product(items, minors)
         for item in items:
             for minor in minors:
                 if self.dims == 3:
@@ -113,53 +109,30 @@ class DataTable(DataFile):
                             self.panel.loc[item, :, minor].values
                         ),
                     })
+                if self.dims == 4:
+                    for major in majors:
+                        data.append({
+                            'item': item,
+                            'minor': minor,
+                            'major': major,
+                            'data_table': self.data_table,
+                            'data': list(
+                                # self.panel.loc[commodity, :, region].values
+                                self.panel.loc[:, item, major, minor].values
+                            ),
+                        })
         return json.dumps(
             data, indent=' ',
         )
 
-    def json_output(self, regions=(), commodities=()):
-        if len(regions) == 0 or len(commodities) == 0:
-            raise Exception(
-                'Need to choose 1 or 2 regions, and 1 or 2 commodities.'
-            )
-        else:
-            if self.data_table in ['expend_sector', 'ratio_sector']:
-                self.panel = self.panel.ix[:26, :30]
-            if self.data_table in ['expend_sector_import', 'ratio_sector_import']:
-                self.panel = self.panel.ix[:26, 30:]
-            if self.data_table in ['expend_region_import', 'ratio_region_import']:
-                self.panel = self.panel.ix[26:, 30:]
-            data = []
-            _reg_sectors = itertools.product(regions, commodities)
-            for region in regions:
-                for commodity in commodities:
-                    if self.dims == 3:
-                        data.append({
-                            'region': region,
-                            'commodity': commodity,
-                            'data_table': self.data_table,
-                            'data': list(
-                                # self.panel.loc[commodity, :, region].values
-                                self.panel.loc[region, :, commodity].values
-                            ),
-                        })
-                    elif self.dims == 4:
-                        data.append({
-                            'region': region,
-                            'commodity': commodity,
-                            'data_table': self.data_table,
-                            'data': list(
-                                self.panel.loc[region, :, commodity, commodity]
-                                .T
-                            ),
-                        })
-            return json.dumps(
-                data, indent=' ',
-            )
-
-if __name__ == "__main__":
-    data_type = 'price'
-    data = DataFile(data_type)
-    json = data.BetterJsonOut(
-        regions=('USA','MEX'), commodities=('OIL',)
-    )
+if __name__ == '__main__':
+    pass
+    c = DataTable('expend_sector_import')
+    print c.get_triple_panel('bottom_left')
+    # c = DataTable('prices')
+    # print c.get_simple_panel()
+    # import timeit
+    # print timeit.timeit('c = DataTable("prices");p = c.get_simple_panel()', setup="from __main__ import DataTable", number=50)
+    # print timeit.timeit('c = DataTable("prices");p = c.panelize()', setup="from __main__ import DataTable", number=50)
+    # print timeit.timeit('c = DataTable("expend_sector_import");p = c.get_triple_panel("bottom_left")', setup="from __main__ import DataTable", number=10)
+    # print timeit.timeit('c = DataTable("expend_sector_import");p = c.panelize()', setup="from __main__ import DataTable", number=10)
