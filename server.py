88e93fe2b9d749803df15c045632eab9d049c24e
Nathan Matteson <matteson@obstructures.org>
2013-06-07 09:50:06 -0500

    Remove static graph titles and call to panelize()

--- server.py
+++ server.py
@@ -11,25 +11,26 @@ def index():
     return render_template('index.html')
 
 @app.route('/json/<_set>/<_table>/<_item>/<_minor>')
-def json2(_set, _table, _item, _minor):
+def json3(_set, _table, _item, _minor):
     _data = DataTable(_table, _data_set=_set)
-    _data.panelize()
     return _data.json_output2(
         items=_item.split(','), minors=_minor.split(',')
     )
 
+@app.route('/json/<_set>/<_table>/<_item>/<_minor>/<_major>')
+def json4(_set, _table, _item, _minor, _major):
+    _data = DataTable(_table, _data_set=_set)
+    return _data.json_output2(
+        items=_item.split(','), minors=_minor.split(','), majors=_major.split(',')
+    )
+
 @app.route('/<data_set>/<data_table>/<check_type>')
 def line_graph(data_set, data_table, check_type):
-    graph_title = {
-        'prices': {'title': 'Change in price', 'unit': '%'},
-        'emissions': {'title': 'Emissions', 'unit': 'Gton'},
-        'emission_flows': {'title': 'Emissions', 'unit': 'Gton'},
-    }
     parser = CimEarthWebParser(data_table)
     _c = None
     _s = []
     for k, v in parser.vectors.iteritems():
-        if v != check_type or _c is not None:
+        if v[0] != check_type or _c is not None:
             try:
                 _s.append(parser.select_menu(v, k))
             except:
